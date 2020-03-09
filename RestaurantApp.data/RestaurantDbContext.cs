f8d3791e14618de665609a37a9d26eb050d91dd0
avnish1981 <avnish.choubey@gmail.com>
2020-03-09 20:10:28 +0530

    Doing Commit for RestauantDBContext

---
+++ RestaurantApp.data/RestaurantDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantApp.core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantApp.data
+{
+    public class RestaurantDbContext :DbContext
+    {
+        public RestaurantDbContext(DbContextOptions<RestaurantDbContext> options ) :base(options )
+        {
+
+        }
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
