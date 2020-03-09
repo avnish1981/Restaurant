7e7e0d7f6fc96895d1145a406cb8c4ffd62f26e2
avnish1981 <avnish.choubey@gmail.com>
2020-03-09 22:00:19 +0530

    Adding new file and migration folder

---
+++ RestaurantApp.data/Migrations/20200309160804_initialcreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestaurantApp.data.Migrations
+{
+    public partial class initialcreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    resId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    resName = table.Column<string>(maxLength: 80, nullable: false),
+                    resLocation = table.Column<string>(maxLength: 255, nullable: false),
+                    CuisinType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.resId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+        }
+    }
+}
---
+++ RestaurantApp.data/clsRestaurantData.cs
@@ -0,0 +1,89 @@
+﻿using RestaurantApp.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestaurantApp.data
+{
+    public class clsRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> objRes = null;
+        public clsRestaurantData()
+        {
+            objRes = new List<Restaurant>
+            {
+                new Restaurant{resId=1,resName="Ïndia Dhaba" , resLocation="Noida",CuisinType=CuisinType.Indian },
+                new Restaurant{resId=2,resName="Punjabi Dhaba" , resLocation="Delhi",CuisinType=CuisinType.Indian },
+                new Restaurant{resId=3,resName="Maxican Curry" , resLocation="Noida",CuisinType=CuisinType.Maxican  },
+                new Restaurant{resId=4,resName="maxican Golf Curry" , resLocation="Noida",CuisinType=CuisinType.Maxican },
+                new Restaurant{resId=5,resName="Suji Italina" , resLocation="Noida",CuisinType=CuisinType.Italian  },
+                new Restaurant{resId=6,resName="GS Italian" , resLocation="Noida",CuisinType=CuisinType.Italian },
+                new Restaurant{resId=7,resName="Holf Maxican" , resLocation="Noida",CuisinType=CuisinType.Maxican },
+                new Restaurant{resId=8,resName="Gujarati Dhaba" , resLocation="Noida",CuisinType=CuisinType.Indian },
+
+
+            };
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+             objRes.Add(newRestaurant);
+            newRestaurant.resId = objRes.Max(r => r.resId) + 1;
+            return newRestaurant;
+        }
+
+        public int commit()
+        {
+            return 0;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = objRes.FirstOrDefault(r => r.resId == id);
+            if(restaurant !=null)
+            {
+                objRes.Remove(restaurant);
+            }
+
+            return restaurant;
+        }
+
+        public IEnumerable<Restaurant> GetAllRestaurantsByName(string resname)
+        {
+            return from r in objRes
+                   where string.IsNullOrEmpty(resname) || r.resName.StartsWith(resname)
+                   orderby r.resName
+                   select r;
+        }
+
+        public Restaurant GetbyId(int id)
+        {
+            return objRes.SingleOrDefault(r => r.resId == id);
+        }
+
+        public Restaurant Updated(Restaurant updatedRestaurant)
+        {
+            var resdata = objRes.SingleOrDefault(r => r.resId == updatedRestaurant.resId);
+            if(resdata !=null)
+            {
+                resdata.resName = updatedRestaurant.resName;
+                resdata.resLocation = updatedRestaurant.resLocation;
+                resdata.CuisinType = updatedRestaurant.CuisinType;
+
+            }
+
+            return resdata;
+
+
+        }
+        /* public IEnumerable<Restaurant> GetAllRestaurants()
+{
+return from r in objRes
+orderby r.resName
+select r;
+
+
+} */
+
+    }
+}
