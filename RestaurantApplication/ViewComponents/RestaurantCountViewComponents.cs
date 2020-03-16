d081098949248040dcafdb0331c5abae6caabe09
avnish1981 <avnish.choubey@gmail.com>
2020-03-16 12:18:28 +0530

    Added these 3 files

---
+++ RestaurantApplication/ViewComponents/RestaurantCountViewComponents.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantApp.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantApplication.ViewComponents
+{
+    public class RestaurantCountViewComponents:ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public RestaurantCountViewComponents(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var count = restaurantData.GetTotalRestaurantCount();
+            return View("Default",count );
+        }
+
+       
+    }
+}
